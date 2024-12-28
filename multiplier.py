b728f81b1ae161e5c7f9d7e6e9a4cf069915c6c1
Komal566523 <komal.-@edu.turkuamk.fi>
2024-12-28 23:28:51 +0200

    file changed and merged

--- multiplier.py
+++ multiplier.py
@@ -13,14 +13,4 @@ plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.show()
 
-import matplotlib.pyplot as plt
-import numpy as np
 
-# Create data for polar coordinates
-theta = np.linspace(0, 2*np.pi, 100)
-r = np.ones_like(theta)
-
-# Create the polar plot
-plt.polar(theta, r, color='orange')
-plt.title("Circle in Polar Coordinates")
-plt.show()
